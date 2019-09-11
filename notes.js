b9580d29092bf77f305ef6527cf5f922195f5d87
bulletTrain54 <mvishnurana@gmail.com>
2019-09-11 10:07:23 +0800

    first commit

---
+++ notes.js
@@ -0,0 +1,59 @@
+const fs = require('fs'); 
+
+const getNotes = function () {
+    return 'Your notes...'; 
+}
+
+const removeNote = function (title) {
+
+    const noteExistence = notes.filter(function(note){
+        return note.title === title; 
+    }); 
+
+    if(noteExistence){
+        console.log('Note with title ', title, ' removed!'); 
+    }
+}
+
+const addNote = function (title, body) {
+
+    // load notes
+    const notes = loadNotes(); 
+
+    // checking for duplicates 
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title; 
+    }); 
+
+    // duplicate does not exist 
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        }); 
+        saveNotes(notes); 
+        console.log('New note added!'); 
+    } else {
+        console.log('Note title taken!'); 
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes); 
+    fs.writeFileSync('notes.json', dataJSON); 
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json'); 
+        const dataJSON = dataBuffer.toString(); 
+        return JSON.parse(dataJSON); 
+    } catch (e) {
+        return []; 
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}; 
